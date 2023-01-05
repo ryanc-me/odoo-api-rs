13f477e86bf071552f70ac7e70289e2bfc486f5c
Ryan Cole <admin@ryanc.me>
2023-01-06 11:27:05 +1300

    Ensure features are gated properly

--- odoo-api/src/client/req_impl/mod.rs
+++ odoo-api/src/client/req_impl/mod.rs
@@ -1,4 +1,8 @@
 pub(crate) mod closure_async;
 pub(crate) mod closure_blocking;
+
+#[cfg(feature = "async")]
 pub(crate) mod reqwest_async;
+
+#[cfg(feature = "blocking")]
 pub(crate) mod reqwest_blocking;
