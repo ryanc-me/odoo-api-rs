68f2b0c150899a74327fc11c01acdb277b311552
Ryan Cole <admin@ryanc.me>
2023-01-06 11:40:26 +1300

    cargo fmt

--- odoo-api/src/client/mod.rs
+++ odoo-api/src/client/mod.rs
@@ -4,16 +4,16 @@
 //!
 //! ##
 
-pub use odoo_client::{AuthState, Authed, NotAuthed, OdooClient, RequestImpl};
-pub use odoo_request::OdooRequest;
 pub use http_impl::closure_async::ClosureResult as AsyncClosureResult;
 pub use http_impl::closure_blocking::ClosureResult as BlockingClosureResult;
+pub use odoo_client::{AuthState, Authed, NotAuthed, OdooClient, RequestImpl};
+pub use odoo_request::OdooRequest;
 
 pub use http_impl::closure_async::ClosureAsync;
 pub use http_impl::closure_blocking::ClosureBlocking;
 pub use http_impl::reqwest_async::ReqwestAsync;
 pub use http_impl::reqwest_blocking::ReqwestBlocking;
 
+mod http_impl;
 mod odoo_client;
 mod odoo_request;
-mod http_impl;
